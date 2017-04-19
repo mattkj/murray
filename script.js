4df3117ec30d89af3d179d06ecb94612201a3939
Matt Johnson <matt@bamsas.com>
2017-04-19 12:20:16 -0700

    start work on replaceText function

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function fillMurray(){
-    console.info('Fill Murraying...');
+function fillMurray() {
+  console.info('Fill Murraying...');
 
-    const images = document.querySelectorAll('img');
-    images.forEach(img => {
-        img.src = `//www.fillmurray.com/${img.width}/${img.height}`;
-    });
+  const images = document.querySelectorAll('img');
+  images.forEach(img => {
+    img.src = `//www.fillmurray.com/${img.width}/${img.height}`;
+  });
+}
+
+function replaceText() {
+  const elements = Array.from(document.querySelector('body').children);
+
+  elements.forEach(element => {
+    if (element.textContent.includes('tv series')){
+      console.log(element.textContent);
+    }
+  });
 }
 
 fillMurray();
