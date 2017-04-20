1d9cca91cb3bdb26810d1f77df8ab0b5e72c35f6
Matt Johnson <computermonkey@gmail.com>
2017-04-19 20:01:57 -0700

    adjust permissions, add getMovies function, start on replaceHeadings function

--- script.js
+++ script.js
@@ -22,5 +22,27 @@ function replaceText() {
   title.textContent = "Bill F*cking Murray";
 }
 
+function replaceHeadings(){
+  const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+  console.log({headings});
+}
+
+function getMovies(){
+  let movies = [];
+
+  console.info('Fetching movies from api...');
+
+  fetch('https://moviesapi.com/m.php?i=0000195&type=person&r=json')
+    .then(response => response.json())
+    .then(data => {
+      movies.push(data.movies_act.split(','));
+    })
+    .catch(error => console.log('Fetch error: ', error.message));
+
+  console.log(movies);
+  
+}
+
 replaceText();
 fillMurray();
+getMovies();
